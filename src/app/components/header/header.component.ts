b38a49b5343a68f46991b0cb1f0e257a4481ecc7
Vitor Caetano <vitorlc95@gmail.com>
2020-12-06 11:31:59 -0300

    Apagando código desnecessário

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Usuario } from 'src/app/models/usuario.model';
-import { LoginService } from 'src/app/services/login.service';
 
 @Component({
   selector: 'app-header',
@@ -12,20 +11,8 @@ export class HeaderComponent implements OnInit {
   usuario: Usuario
   isAdmin: boolean = true
 
-  constructor(private loginService: LoginService) {
-    // this.usuario = this.loginService.getUsuarioLogado()
-
-    // if(this.usuario) {
-    //   this.isAdmin = this.usuario.admin
-    // } else {
-    //   this.isAdmin = false
-    // }
-
-   }
+  constructor() { }
 
   ngOnInit(): void {
   }
-
-
-
 }
